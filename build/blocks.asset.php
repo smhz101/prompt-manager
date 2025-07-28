0cafbc709af46ea40fff93db9fe8b16b1e309bc0
Muzammil Hussain <smhz101@users.noreply.github.com>
2025-07-28 16:20:45 +0500

    chore: regenerate block asset dependencies

--- build/blocks.asset.php
+++ build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'a07b0c2691c071208602');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2a5eb3ea25d7518ee86d');
